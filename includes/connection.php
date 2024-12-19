862e0ec0ec26c8f67f8fa2c00ddbf9237be302a2
Akraja712 <ashokkumar7hitter@gmail.com>
2024-12-19 18:24:41 +0530

    connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,4 +1,4 @@
 <?php
-define('DOMAIN_URL', 'http://localhost/solar_grids/');
-define('API_URL', 'http://localhost/solar_grids/admin_v1/api/');
+define('DOMAIN_URL', 'https://solargrids.graymatterworks.com/');
+define('API_URL', 'https://solargrids.graymatterworks.com/admin_v1/api/');
 ?>
