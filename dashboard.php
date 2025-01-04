0319b93f81f73dbd07898b7f7a1d38b68f0c13e2
Akraja712 <ashokkumar7hitter@gmail.com>
2025-01-04 12:51:31 +0530

    dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -310,7 +310,7 @@ curl_close($curl);
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body text-center">
-                    <img src="discount.png" alt="New Year Offer" class="img-fluid" style="max-width: 700px; height: 300px;">
+                    <img src="discount.png" alt="New Year Offer" class="img-fluid" style="max-width: 900px; height: 300px;">
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
