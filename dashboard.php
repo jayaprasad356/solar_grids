aaa8c2642173fe830361b3c3e7255473b739dca8
karthiammu143 <karthikeyanammu440@gmail.com>
2025-01-13 12:24:34 +0530

    dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -340,7 +340,7 @@ if ($response === false) {
         <div class="modal-dialog modal-dialog-centered modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="offerModalLabel">Special New Year Offer!</h5>
+                    <!-- <h5 class="modal-title" id="offerModalLabel">Special New Year Offer!</h5> -->
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body text-center">
