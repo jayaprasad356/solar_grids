b2fa600df665ea92296c7edc7a4c4517ec4c9644
Akraja712 <ashokkumar7hitter@gmail.com>
2025-01-29 22:57:10 +0530

    crud.php

--- main_controlv3/includes/crud.php
+++ main_controlv3/includes/crud.php
@@ -18,9 +18,9 @@ class Database
 
          //  live server
          private $db_host = "localhost";
-         private $db_user = "root";
-         private $db_pass = ""; 
-         private $db_name = "solar_grids"; 
+         private $db_user = "u117947056_solarpe";
+         private $db_pass = "Solarpe@2024"; 
+         private $db_name = "u117947056_solarpe"; 
  
     // demo server
     // private $db_host = "localhost";
